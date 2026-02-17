8d3b2f78ebdcde3dafbf441463a9ee303c2d04a4
AbhishekChavan1 <abhishekchavan2714@gmail.com>
2026-02-17 18:37:00 +0530

    Initial commit for FastAPI project

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello World"}
 
+def hi():
+    return "hi"
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
